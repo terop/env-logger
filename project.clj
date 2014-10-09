107fde130ce43c18f140f8f5c269e147fc1d1eb8
Tero Paloheimo <tero.paloheimo@iki.fi>
2014-10-09 20:58:20 +0300

    Updated project dependencies

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
 (defproject env-logger "0.1.0-SNAPSHOT"
 	:description "A simple data logger"
 	:url "http://example.com/FIXME"
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [http-kit "2.1.18"]
-                   [ring "1.3.0"]
-                   [compojure "1.1.8"]
-                   [postgresql "9.3-1101.jdbc4"]
-                   [korma "0.3.2"]
-                   [clj-time "0.7.0"]
-                   [org.clojure/data.json "0.2.5"]
-                   [enlive "1.1.5"]]
-  :main env-logger.main
-  :plugins [[lein-ring "0.8.10"]]
-  :ring {:handler env-logger.main/main
-         :auto-reload? true}
-  :profiles
-  {:dev {:resource-paths ["resources"]}})
+        :dependencies [[org.clojure/clojure "1.6.0"]
+                       [http-kit "2.1.19"]
+                       [ring "1.3.1"]
+                       [compojure "1.2.0"]
+                       [postgresql "9.3-1101.jdbc4"]
+                       [korma "0.4.0"]
+                       [clj-time "0.8.0"]
+                       [org.clojure/data.json "0.2.5"]
+                       [enlive "1.1.5"]]
+        :main env-logger.main
+        :plugins [[lein-ring "0.8.10"]]
+        :ring {:handler env-logger.main/main
+               :auto-reload? true}
+        :profiles
+        {:dev {:resource-paths ["resources"]}})
