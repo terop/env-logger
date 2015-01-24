f67f4589768eb9886d350c54df221544e8500e46
Tero Paloheimo <tero.paloheimo@iki.fi>
2015-01-24 15:37:01 +0200

    Updated Clojure and JS dependencies and changed time formatting
    Also fixed indentation in some files.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject env-logger "0.1.0-SNAPSHOT"
 	:description "A simple data logger"
-	:url "http://example.com/FIXME"
+	:url "https://github.com/terop/env-logger"
         :dependencies [[org.clojure/clojure "1.6.0"]
                        [http-kit "2.1.19"]
-                       [ring "1.3.1"]
-                       [compojure "1.2.0"]
+                       [ring "1.3.2"]
+                       [compojure "1.3.1"]
                        [postgresql "9.3-1101.jdbc4"]
                        [korma "0.4.0"]
-                       [clj-time "0.8.0"]
+                       [clj-time "0.9.0"]
                        [org.clojure/data.json "0.2.5"]
                        [enlive "1.1.5"]]
         :main env-logger.main
