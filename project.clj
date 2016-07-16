d3eeed3729f23ad71568682e6c5cc2ee32db4aba
Tero Paloheimo <tero.paloheimo@iki.fi>
2016-07-16 15:16:36 +0300

    Update Leiningen dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "A simple data logger"
   :url "https://github.com/terop/env-logger"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.immutant/web "2.1.3"]
-                 [compojure "1.5.0"]
-                 [ring "1.4.0"]
-                 [ring/ring-defaults "0.2.0"]
-                 [org.postgresql/postgresql "9.4.1207"]
+                 [org.immutant/web "2.1.5"]
+                 [compojure "1.5.1"]
+                 [ring "1.5.0"]
+                 [ring/ring-defaults "0.2.1"]
+                 [org.postgresql/postgresql "9.4.1209"]
                  [korma "0.4.2"]
-                 [clj-time "0.11.0"]
-                 [cheshire "5.5.0"]
-                 [selmer "1.0.2"]
+                 [clj-time "0.12.0"]
+                 [cheshire "5.6.3"]
+                 [selmer "1.0.7"]
                  [log4j "1.2.17" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
