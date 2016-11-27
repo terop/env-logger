823174efb6377bcc4efa4939ad853bd28517c440
Tero Paloheimo <tero.paloheimo@iki.fi>
2016-11-27 14:41:11 +0200

    Change plotting library to Google Charts

--- src/public/js/plots.js
+++ src/public/js/plots.js
@@ -5,6 +5,9 @@ var transformData = function (data) {
         plotData = [],
         dataPoint = [];
 
+    // Data labels
+    plotData.push(['Date', 'Temperature [\xB0C]', 'Brightness',
+                   'Temperature (outside) [\xB0C]', 'Cloudiness']);
     for (var i = 0; i < dataJson.length; i++) {
         dataPoint = [new Date(dataJson[i]['recorded']),
                      dataJson[i]['temperature'],
@@ -17,36 +20,79 @@ var transformData = function (data) {
 };
 var plotData = transformData(document.getElementById('plotData').innerHTML);
 
-if (plotData.length > 0) {
-    var g = new Dygraph(
-        document.getElementById('tempChart'),
-        plotData,
-        {
-            labels: ['Date', 'Temperature [\xB0C]', 'Brightness',
-                     'Temperature (outside) [\xB0C]', 'Cloudiness'],
+if (plotData.length > 1) {
+    google.charts.load('current', {'packages': ['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var options = {
+            width: 900,
+            height: 450,
+            chartArea: {
+                width: 800
+            },
+            hAxis: {
+                gridlines: {
+                    count: -1,
+                    units: {
+                        days: {format: ['d.M.yyyy']},
+                        hours: {format: ['HH:mm']}
+                    }
+                },
+                minorGridlines: {
+                    units: {
+                        hours: {format: ['hh:mm']},
+                        minutes: {format: ['HH:mm']}
+                    }
+                }
+            },
             title: 'Observations',
-            labelsDiv: 'legendDiv',
-            labelsSeparateLines: true
-        });
+            legend: { position: 'bottom' },
+            explorer: {
+                actions: ['dragToZoom', 'rightClickToReset'],
+                keepInBounds: true
+            }
+        },
+            data = google.visualization.arrayToDataTable(
+                plotData,
+                false),
+            dateFormatter = new google.visualization.DateFormat({
+                pattern: 'd.M.yyyy HH:mm:ss'
+            });
 
+        dateFormatter.format(data, 0);
+        var chart = new google.visualization.LineChart(document.getElementById('chartDiv'));
+        chart.draw(data, options);
 
-    // Hides or shows the selected data series
-    var hideOrShowSeries = function (event) {
-        var mapping = {'showTemperature': 0,
-                       'showBrightness': 1,
-                       'showOutsideTemperature': 2,
-                       'showCloudiness': 3};
+        // Hides or shows the selected data series
+        var hideOrShowSeries = function (event) {
+            var mapping = {'showTemperature': 1,
+                           'showBrightness': 2,
+                           'showOutsideTemperature': 3,
+                           'showCloudiness': 4},
+                shownColumns = [0];
 
-        var keys = Object.keys(mapping);
-        for (var i = 0; i < keys.length; i++) {
-            g.setVisibility(mapping[keys[i]],
-                            document.getElementById(keys[i]).checked);
+            var keys = Object.keys(mapping);
+            for (var i = 0; i < keys.length; i++) {
+                if (document.getElementById(keys[i]).checked) {
+                    shownColumns.push(mapping[keys[i]]);
+                }
+            }
+
+            if (shownColumns.length == 1) {
+                alert('At least one data series must be selected');
+                return;
+            }
+
+            view = new google.visualization.DataView(data);
+            view.setColumns(shownColumns);
+            chart.draw(view, options);
+        };
+
+        var checkboxes = document.getElementsByClassName('selectBox');
+        for (var i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].addEventListener('click', hideOrShowSeries);
         }
-    };
-
-    var checkboxes = document.getElementsByClassName('selectBox');
-    for (var i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener('click', hideOrShowSeries);
     }
 } else {
     document.getElementById('noDataError').style.display = 'inline';
