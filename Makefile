f23c28b5ae4de72dbf75e021021df9b6c3f7e731
Tero Paloheimo <tero.paloheimo@iki.fi>
2016-12-13 08:11:26 +0200

    Release 0.2.0
    Features:
    * Authentication with password or Yubikey OTP
    * Change plotting library to Google Charts

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 build: # build container
 	lein uberjar
-	docker build -t env-logger .
+	docker build -t env-logger:0.2.0 .
 
 clean:
 	lein clean
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject env-logger "0.1.1-SNAPSHOT"
+(defproject env-logger "0.2.0"
   :description "A simple data logger"
   :url "https://github.com/terop/env-logger"
   :dependencies [[org.clojure/clojure "1.8.0"]
