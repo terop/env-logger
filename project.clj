e2892e1cbf4708d938c6e782b384d533180579f8
Tero Paloheimo <tero.paloheimo@iki.fi>
2017-02-18 20:32:53 +0200

    Release v0.2.4
    Improvements:
    * Fix BLE beacon scanning
    * Change OTP field type to password

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject env-logger "0.2.4-SNAPSHOT"
+(defproject env-logger "0.2.4"
   :description "A simple data logger"
   :url "https://github.com/terop/env-logger"
   :dependencies [[org.clojure/clojure "1.8.0"]
