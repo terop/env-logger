d63e4a337a2ef1ea4eec4f529ceae97f4e589604
Tero Paloheimo <tero.paloheimo@iki.fi>
2017-03-28 16:44:08 +0300

    Start work on version 0.2.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject env-logger "0.2.5"
+(defproject env-logger "0.2.6-SNAPSHOT"
   :description "A simple data logger"
   :url "https://github.com/terop/env-logger"
   :dependencies [[org.clojure/clojure "1.8.0"]
