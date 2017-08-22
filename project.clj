111f34c45f1f39b9e266bb9154f708881f6e2e2c
Tero Paloheimo <tero.paloheimo@iki.fi>
2017-08-22 19:12:42 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -10,14 +10,14 @@
                  [org.clojure/java.jdbc "0.7.0"]
                  [honeysql "0.9.0"]
                  [clj-time "0.14.0"]
-                 [cheshire "5.7.1"]
+                 [cheshire "5.8.0"]
                  [selmer "1.11.0"]
-                 [clj-http "3.6.1"]
+                 [clj-http "3.7.0"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.25"]
                  [com.yubico/yubico-validation-client2 "3.0.1"]
                  [buddy/buddy-hashers "1.2.0"]
-                 [buddy/buddy-auth "1.4.1"]
+                 [buddy/buddy-auth "2.0.0"]
                  [org.clojars.pntblnk/clj-ldap "0.0.12"]]
   :main env-logger.handler
   :aot [env-logger.handler
