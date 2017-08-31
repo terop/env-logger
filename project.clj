db1e77f095cbc80d6e6670f5b1b385cb010e6d15
Tero Paloheimo <tero.paloheimo@iki.fi>
2017-08-31 18:17:53 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
                  [ring "1.6.2"]
                  [ring/ring-defaults "0.3.1"]
                  [org.postgresql/postgresql "42.1.4"]
-                 [org.clojure/java.jdbc "0.7.0"]
-                 [honeysql "0.9.0"]
+                 [org.clojure/java.jdbc "0.7.1"]
+                 [honeysql "0.9.1"]
                  [clj-time "0.14.0"]
                  [cheshire "5.8.0"]
                  [selmer "1.11.0"]
@@ -16,8 +16,8 @@
                  [org.clojure/tools.logging "0.4.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.25"]
                  [com.yubico/yubico-validation-client2 "3.0.1"]
-                 [buddy/buddy-hashers "1.2.0"]
-                 [buddy/buddy-auth "2.0.0"]
+                 [buddy/buddy-hashers "1.3.0"]
+                 [buddy/buddy-auth "2.1.0"]
                  [org.clojars.pntblnk/clj-ldap "0.0.12"]]
   :main env-logger.handler
   :aot [env-logger.handler
