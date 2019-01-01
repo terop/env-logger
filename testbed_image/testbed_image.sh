91427308b67c27d235bbfb33e44ac07e5d96acd5
Tero Paloheimo <tero.paloheimo@iki.fi>
2019-01-01 12:10:23 +0200

    Improve Testbed image download script

--- testbed_image/testbed_image.sh
+++ testbed_image/testbed_image.sh
@@ -2,34 +2,41 @@
 
 # A script for uploading a FMI Testbed image to a remote server.
 #
-# Copyright (C) 2017 Tero Paloheimo
+# Copyright (C) 2017-2019 Tero Paloheimo
 
-# IMPORTANT! Change these values before running!
-ssh_con_settings="user@host"
-image_dir_basename="/home/user/some/location"
+# Check configuration file existence
+conf_file='testbed_image_conf.sh'
+if [ ! -e ${conf_file} ]; then
+    echo "Configuration file '${conf_file}' not found, exiting." >&2
+    exit 1
+fi
+. ./${conf_file}
 
-# Ensure correct base directory
-# ssh $ssh_con_settings 'mkdir $image_dir_basename'
+ssh_con_string="${target_user}@${target_host}"
 
 image_name=$(python3 testbed_image.py)
-if [ -z "$image_name" ]; then
-    echo "$(date -Iminutes): Failed to download Testbed image." >&2
+if [ -z "${image_name}" ]; then
+    echo "$(date -Iminutes): failed to download Testbed image." >&2
     exit 1
 fi
 
-day_directory="$image_dir_basename/$(date +%Y-%m-%d)"
+day_directory="${target_directory}/$(date +%Y-%m-%d)"
 # Make directory for each day
-ssh $ssh_con_settings "ls $day_directory" > /dev/null
+ssh "${ssh_con_string}" "ls ${day_directory}" >/dev/null
 if [ $? -ne 0 ]; then
-    ssh $ssh_con_settings "mkdir $day_directory"
+    ssh "${ssh_con_string}" "mkdir ${day_directory}"
 fi
 
-scp ./"$image_name" "$ssh_con_settings:$day_directory/$image_name"
+scp ./"${image_name}" "${ssh_con_string}:${day_directory}/${image_name}" 1>/dev/null
 if [ $? -ne 0 ]; then
-    echo "$(date -Iminutes): Image upload failed, exiting." >&2
+    echo "$(date -Iminutes): image upload failed, exiting." >&2
+    rm "${image_name}"
+    exit 1
 fi
-rm "$image_name"
+rm "${image_name}"
 
 # Add image name to env-logger database
-# auth_code=somebadvalue
-# curl --data-urlencode "name=$image_name" --data-urlencode "code=$auth_code" https://example.com/env-logger/tb-image
+if [ ${env_logger_integration} -ne 0 ]; then
+    curl -s -S --data-urlencode "name=${image_name}" --data-urlencode "code=${auth_code}" \
+         "${env_logger_url}/tb-image" 1>/dev/null
+fi
