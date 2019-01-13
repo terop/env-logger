1b1f2bd2d3d39ae747805cb83966f2cba6ffc2a5
Tero Paloheimo <tero.paloheimo@iki.fi>
2019-01-13 19:41:56 +0200

    Rename DB restore script and improve help message

--- scripts/local_db_restore.sh
+++ scripts/local_db_restore.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-# This a script for updating the local env-logger database from a backup snapshot.
+# This a script for restoring the local env-logger database from a backup snapshot.
 
 set -e
 
 if [ $# -ne 2 ]; then
-    echo "Usage: $0 <DB name> <DB snapshot name>"
-    echo "Example: $0 env_logger db_snapshot.sql"
+    cat <<EOF
+This a script for restoring the local env-logger database from a backup snapshot.
+
+Usage: $0 <DB name> <DB snapshot name>
+Example: $0 env_logger db_snapshot.sql
+EOF
     exit 1
 fi
 
@@ -21,5 +25,7 @@ TRUNCATE TABLE yardcam_images;
 EOF
 
 echo "Adding new values"
+# Pressure data has not been collected from the beginning and thus contains
+# NULL values causing restore to fail
 psql "${db_name}" -c 'ALTER TABLE weather_data ALTER pressure DROP NOT NULL;'
 psql "${db_name}" < "${snapshot_name}"
