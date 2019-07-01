de1c573ba44a87d4b7e00a6e1314fccca936d7e0
Tero Paloheimo <tero.paloheimo@iki.fi>
2019-07-01 19:30:57 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,23 +1,23 @@
 (defproject env-logger "0.2.11-SNAPSHOT"
   :description "A simple data logger"
   :url "https://github.com/terop/env-logger"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.immutant/web "2.1.10"]
                  [compojure "1.6.1"]
                  [ring "1.7.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [org.postgresql/postgresql "42.2.5"]
+                 [org.postgresql/postgresql "42.2.6"]
                  [org.clojure/java.jdbc "0.7.9"]
                  [honeysql "0.9.4"]
                  [clj-time "0.15.1"]
                  [cheshire "5.8.1"]
                  [selmer "1.12.12"]
-                 [clj-http "3.9.1"]
+                 [clj-http "3.10.0"]
                  [org.clojure/tools.logging "0.4.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.26"]
                  [com.yubico/yubico-validation-client2 "3.0.5"]
-                 [buddy/buddy-hashers "1.3.0"]
-                 [buddy/buddy-auth "2.1.0"]
+                 [buddy/buddy-hashers "1.4.0"]
+                 [buddy/buddy-auth "2.2.0"]
                  [org.clojars.pntblnk/clj-ldap "0.0.16"]
                  [org.influxdb/influxdb-java "2.15"]]
   :main env-logger.handler
