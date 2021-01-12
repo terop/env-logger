12d25874484385a2c3d6ef21d0afa9d767e87b93
Tero Paloheimo <tero.paloheimo@iki.fi>
2021-01-12 19:48:36 +0200

    Update Wio Terminal code
    Remove also WiFi server support as unreliable and rely on serial
    reading.

--- wioterminal.ino
+++ wioterminal.ino
@@ -1,6 +1,3 @@
-#include <rpcWiFi.h>
-#include <WebServer.h>
-
 #include <Wire.h>
 #include "SHT31.h"
 
@@ -10,27 +7,25 @@
  * Required libraries:
  * ArduinoJson
  * Grove_SHT31_Temp_Humi_Sensor
- * Seeed_Arduino_FS
- * Seeed_Arduino_mbedtls
- * Seeed_Arduino_rpcUnified
- * Seeed_Arduino_rpcWiFi
- * Seeed_Arduino_SFUD
  */
 
-// CHANGE THESE before running!
-const char* SSID = "MyNetwork";
-const char* PASSWORD = "MyPassword";
-
-WebServer server(80);
 SHT31 sht31 = SHT31();
 
 const int ledPin = 13;
 
 // Reads the sensors and returns values as JSON
 String readSensors() {
+  digitalWrite(ledPin, 1);
+
+  const uint8_t lightMeasCount = 4;
+  int lightValuesSum = 0;
+
+  for (uint8_t i = 0; i < lightMeasCount; i++)
+    lightValuesSum += analogRead(A0);
+
   float temperature = sht31.getTemperature();
   float humidity = sht31.getHumidity();
-  int lightValue = analogRead(A0);
+  int lightValue = round((float)lightValuesSum / lightMeasCount);
   int builtInLightValue = analogRead(WIO_LIGHT);
 
   delay(400);
@@ -45,58 +40,24 @@ String readSensors() {
 
   serializeJson(doc, output);
 
+  digitalWrite(ledPin, 0);
+
   return output;
 }
 
-void handleRoot() {
-  digitalWrite(ledPin, 1);
-
-  server.send(200, "application/json", readSensors());
-
-  digitalWrite(ledPin, 0);
-}
-
-void handleNotFound() {
-  digitalWrite(ledPin, 1);
-
-  server.send(404, "text/plain", "File Not Found\n\n");
-
-  digitalWrite(ledPin, 0);
-}
-
 void setup(void) {
+  pinMode(A0, INPUT);
   pinMode(WIO_LIGHT, INPUT);
   pinMode(ledPin, OUTPUT);
 
-  digitalWrite(ledPin, 0);
+  digitalWrite(ledPin, 1);
 
   Serial.begin(115200);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, PASSWORD);
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.print("Connected to ");
-  Serial.println(SSID);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
   sht31.begin();
-
-  server.on("/", handleRoot);
-
-  server.onNotFound(handleNotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
 }
 
 void loop(void) {
   Serial.println(readSensors());
-  server.handleClient();
-  delay(2000);
+  delay(4000);
 }
