ce9359c7097a5b052b735bfe96d4dcdbbb4b7416
Tero Paloheimo <tero.paloheimo@iki.fi>
2021-05-08 15:29:59 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -13,14 +13,14 @@
                  [clojure.java-time "0.3.2"]
                  [org.threeten/threeten-extra "1.6.0"]
                  [cheshire "5.10.0"]
-                 [selmer "1.12.34"]
+                 [selmer "1.12.40"]
                  [clj-http "3.12.1"]
                  [org.clojure/tools.logging "1.1.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.30"]
                  [com.yubico/yubico-validation-client2 "3.1.0"]
-                 [buddy/buddy-hashers "1.8.0"]
-                 [buddy/buddy-auth "2.2.0"]
-                 [buddy/buddy-sign "3.4.0"]
+                 [buddy/buddy-hashers "1.8.1"]
+                 [buddy/buddy-auth "3.0.1"]
+                 [buddy/buddy-sign "3.4.1"]
                  [org.clojars.pntblnk/clj-ldap "0.0.17"]]
   :main ^:skip-aot env-logger.handler
   :aot [env-logger.handler
