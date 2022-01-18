6ba065a8a1bff0b1eaf4e1993eb772c8e214b681
Tero Paloheimo <tero.paloheimo@iki.fi>
2022-01-18 07:30:11 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.cache "1.0.225"]
                  [org.clojure/data.zip "1.0.0"]
-                 [ring "1.9.4"]
+                 [ring "1.9.5"]
                  [ring/ring-defaults "0.3.3"]
                  [ring/ring-json "0.5.1"]
                  [com.taoensso/timbre "5.1.2"]
@@ -19,11 +19,11 @@
                  [selmer "1.12.49"]
                  [clj-http "3.12.3"]
                  [com.yubico/yubico-validation-client2 "3.1.0"]
-                 [buddy/buddy-hashers "1.8.1"]
-                 [buddy/buddy-auth "3.0.1"]
-                 [buddy/buddy-sign "3.4.1"]
+                 [buddy/buddy-hashers "1.8.158"]
+                 [buddy/buddy-auth "3.0.323"]
+                 [buddy/buddy-sign "3.4.333"]
                  ;; Used by dependencies, not the app itself
-                 [org.slf4j/slf4j-log4j12 "1.7.32"]]
+                 [org.slf4j/slf4j-log4j12 "1.7.33"]]
   :main ^:skip-aot env-logger.handler
   :aot [env-logger.handler]
   :target-path "target/%s/"
