terop/env-logger

commit 83b120142e99d5d2be24fbdb2881ae09bd0ba752

Rework security settings
Also update java-time version.

src/env_logger/handler.clj
@@ -219,26 +219,28 @@
   []
   (let [port (Integer/parseInt (get (System/getenv)
                                     "APP_PORT" "8080"))
-        force-https (:force-https env)
-        defaults (if force-https
-                   secure-site-defaults
-                   site-defaults)
+        opts {:port port}
+        dev-mode (:development-mode env)
+        defaults (if dev-mode
+                   site-defaults
+                   secure-site-defaults)
         ;; CSRF protection is knowingly not implemented
-        defaults-config (assoc-in (assoc defaults
-                                         :proxy (:use-proxy env))
-                                  [:security :anti-forgery] false)
+        defaults-config (assoc-in defaults [:security :anti-forgery] false)
         handler (as-> routes $
                   (wrap-authorization $ auth/auth-backend)
-                  (wrap-authentication $
-                                       auth/jwe-auth-backend
-                                       auth/auth-backend)
-                  (wrap-defaults $ defaults-config)
+                  (wrap-authentication $ auth/auth-backend)
                   (wrap-json-response $ {:pretty false})
-                  (wrap-json-params $ {:keywords? true}))
-        opts {:port port}]
-    ;; Load initial weather data
-    (fetch-all-weather-data)
-    (run-jetty (if force-https
-                 handler
-                 (wrap-reload handler))
+                  (wrap-json-params $ {:keywords? true})
+                  (wrap-defaults $ (if dev-mode
+                                     defaults-config
+                                     (if (:force-hsts env)
+                                       (assoc defaults-config :proxy true)
+                                       (-> defaults-config
+                                           (assoc-in [:security :ssl-redirect]
+                                                     false)
+                                           (assoc-in [:security :hsts]
+                                                     false))))))]
+    (run-jetty (if dev-mode
+                 (wrap-reload handler)
+                 handler)
                opts)))

deps.edn
@@ -7,7 +7,7 @@
         ring/ring-json {:mvn/version "0.5.1"}
         clj-http/clj-http {:mvn/version "3.12.3"}
         io.github.dm3/clojure.java-time
-        {:sha "2a86c6595f6351dcf73d3a94a27c2ef3f4595644"}
+        {:sha "6a56f6c144c296efded2872ebb127d4b2e05ec9c"}
         org.threeten/threeten-extra {:mvn/version "1.7.0"}
         compojure/compojure {:mvn/version "1.7.0"}
         selmer/selmer {:mvn/version "1.12.53"}

resources/config.edn_sample
@@ -35,10 +35,12 @@
             :hostname "localhost"
             ;; Use HTTPS?
             :use-https true}
- ;; Force settings for HTTPS access without prior HTTPS stripping
- :force-https false
- ;; Use through proxy (usually needed when reverse proxies are used)
- :use-proxy true
+ ;; Enable enforcing of HSTS, only use if HTTPS is not terminated by a reverse
+ ;; proxy or load balancer
+ :force-hsts false
+ ;; Enable development mode, it enables the wrap-reload middleware and
+ ;; uses the site-defaults config
+ :development-mode false
  ;; Full URL through which the application is accessed, MUST end with a slash
  :application-url "http://example.com/env-logger/"
  ;; Base path to Testbed images

commit ce3867e1713dad3cf6350a8882a0d0dec316ab31

Fix broken token authentication
This was accidentally broken in commit 83b1201.
Also improve a function behaviour when there is no data.

src/env_logger/handler.clj
@@ -54,14 +54,16 @@
                                                       (t/minutes 45))
                                              (t/local-date-time)
                                              (:ruuvitag-locations env)))))]
-        {:status 200
-         :body {:data (assoc data :recorded
-                             (convert-epoch-ms-to-string (:recorded data)))
-                :rt-data (for [item rt-data]
-                           (assoc item :recorded
-                                  (convert-epoch-ms-to-string
-                                   (:recorded item))))
-                :weather-data (get-fmi-weather-data)}}))))
+        (if-not data
+          {:status 500}
+          {:status 200
+           :body {:data (assoc data :recorded
+                               (convert-epoch-ms-to-string (:recorded data)))
+                  :rt-data (for [item rt-data]
+                             (assoc item :recorded
+                                    (convert-epoch-ms-to-string
+                                     (:recorded item))))
+                  :weather-data (get-fmi-weather-data)}})))))
 
 (defn get-plot-page-data
   "Returns data needed for rendering the plot page."
@@ -228,7 +230,9 @@
         defaults-config (assoc-in defaults [:security :anti-forgery] false)
         handler (as-> routes $
                   (wrap-authorization $ auth/auth-backend)
-                  (wrap-authentication $ auth/auth-backend)
+                  (wrap-authentication $
+                                       auth/jwe-auth-backend
+                                       auth/auth-backend)
                   (wrap-json-response $ {:pretty false})
                   (wrap-json-params $ {:keywords? true})
                   (wrap-defaults $ (if dev-mode