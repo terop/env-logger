07dbb611aacbf7b0b255d68db03dac131e074cd8
Tero Paloheimo <tero.paloheimo@iki.fi>
2022-10-01 13:01:04 +0300

    electricity price: use different scraping source

--- electricity_price/spot_price/spot_price/spiders/spotpricespider.py
+++ electricity_price/spot_price/spot_price/spiders/spotpricespider.py
@@ -5,25 +5,31 @@ from datetime import date, datetime, time
 
 import scrapy
 
+VAT_MULTIPLIER = 1.24
+
 
 class SpotPriceSpider(scrapy.Spider):
     """Main spider class."""
     name = 'spot_price'
 
     def start_requests(self):
-        yield scrapy.Request(url='https://www.sahkon-kilpailutus.fi/porssisahkon-hinta/',
+        yield scrapy.Request(url='https://www.herrfors.fi/fi/spot-hinnat/',
                              callback=self.parse)
 
     def parse(self, response):  # pylint: disable=arguments-differ
-        table = response.xpath('//table[@id="supsystic-table-2"]')
-        if not table:
-            logging.error('Could not find data table')
+        price_list = response.xpath('//ul[@class="spotprice-list mt-3"]')[1].css('li')
+
+        if not price_list:
+            logging.error('Could not find price list')
             return
 
         today = date.today()
-        for row in table[0].css('tr')[2:28]:
+        for item in price_list[1:25]:
             yield {
-                'time': datetime.combine(today, time(hour=int(row.css('td::text')[0].get()
-                                                              .strip().split('-')[0]))).isoformat(),
-                'price': row.css('td::text')[1].get().strip()
+                'time': datetime.combine(today,
+                                         time(hour=int(item.css('li').css('span::text')[0].
+                                                       get().strip().split(' ')[0]))).isoformat(),
+                # Price is without VAT so it is manually added
+                'price': round(float(item.css('li').css('span')[1].css('pricedata').
+                                     attrib['data-price']) * VAT_MULTIPLIER, 2)
             }
