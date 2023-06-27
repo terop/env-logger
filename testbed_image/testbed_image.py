a23912f0df9a0a0a7579e4831c8961ab38500bfe
Tero Paloheimo <tero.paloheimo@iki.fi>
2023-06-27 09:20:08 +0300

    testbed image: fix errors reported by ruff
    Also update a dependency.

--- testbed_image/testbed_image.py
+++ testbed_image/testbed_image.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
-"""A script for downloading the current FMI Testbed image. Prints an empty string on failure
-and a filename where the latest Testbed image is stored on success."""
+"""A script for downloading the current FMI Testbed image.
+
+Prints an empty string on failure and a filename where the latest Testbed image
+is stored on success.
+"""
 
 import sys
 from datetime import datetime
 
 import pytz
 import requests
-from bs4 import BeautifulSoup  # pylint: disable=import-error
+from bs4 import BeautifulSoup
 
 
 def download_image():
-    """Downloads the latest FMI Testbed image. Returns the image data on success
-    and None otherwise."""
+    """Download the latest FMI Testbed image.
+
+    Returns the image data on success and None otherwise.
+    """
     timestamp = datetime.now().isoformat()
     try:
         resp = requests.get('http://testbed.fmi.fi/?imgtype=radar&t=5&n=1')
-    # pylint: disable=invalid-name
     except requests.ConnectionError as ce:
         print(f'{timestamp}: Failed to access Testbed page: {ce}',
               file=sys.stderr)
@@ -25,7 +29,6 @@ def download_image():
     if not resp.ok:
         return None
 
-    # pylint: disable=invalid-name
     bs = BeautifulSoup(resp.text, 'lxml')
     images = bs.find_all(id='anim_image_anim_anim')
 
@@ -38,7 +41,6 @@ def download_image():
 
     try:
         resp = requests.get(img_url)
-    # pylint: disable=invalid-name
     except requests.ConnectionError as ce:
         print(f'{timestamp}: Failed to download Testbed image: {ce}',
               file=sys.stderr)
@@ -48,20 +50,20 @@ def download_image():
 
 
 def main():
-    """Module main function."""
+    """Run the module code."""
     image = download_image()
     if not image:
         print('')
         return 1
 
-    helsinki = pytz.timezone('Europe/Helsinki')
-    filename = f'testbed-{helsinki.localize(datetime.now()).strftime("%Y-%m-%dT%H:%M%z")}.png'
+    helsinki_tz = pytz.timezone('Europe/Helsinki')
+    filename_dt = helsinki_tz.localize(datetime.now()).strftime('%Y-%m-%dT%H:%M%z')
+    filename = f'testbed-{filename_dt}.png'
 
     with open(filename, 'wb') as tb_image:
         tb_image.write(image)
 
     print(filename)
-    return 0
 
 
 main()
