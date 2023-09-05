badd5b81e3095b1146f73f1b8e63853e7f591d67
Tero Paloheimo <tero.paloheimo@iki.fi>
2023-09-05 19:50:42 +0300

    pyportal: fix electricity price data dict key

--- pyportal/code.py
+++ pyportal/code.py
@@ -220,7 +220,7 @@ def prepare_elec_price_data(elec_price_data, utc_offset_hours):
 
     prices = OrderedDict()
     tz_delta = timedelta(hours=utc_offset_hours)
-    for item in elec_price_data['data']:
+    for item in elec_price_data['data-hour']:
         prices[datetime.fromisoformat(item['start-time'].replace('Z', '')) + \
                tz_delta] = item['price']
 
