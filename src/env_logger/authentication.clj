8e9fe41c0cd98d08947dfc1b44ebc815c4904fca
Tero Paloheimo <tero.paloheimo@iki.fi>
2024-11-27 18:18:40 +0200

    backend: update splint and fix a new splint warning

--- src/env_logger/authentication.clj
+++ src/env_logger/authentication.clj
@@ -224,10 +224,9 @@
   (let [auth-data (:data-user-auth-data env)
         username (get-in request [:params "username"])
         password (get-in request [:params "password"])
-        valid? (and (and username
-                         (= username (:username auth-data)))
-                    (and password
-                         (h/check password (:password auth-data))))]
+        valid? (and (and username (= username (:username auth-data)))
+                    password
+                    (h/check password (:password auth-data)))]
     (if valid?
       (let [claims {:user (keyword username)
                     :exp (Instant/.getEpochSecond
