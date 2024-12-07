866405b818b0d47faa638f9853dae9b04997f6ce
Tero Paloheimo <tero.paloheimo@iki.fi>
2024-12-07 18:06:30 +0200

    wioterminal: remove HTTP server support
    I cannot get the WiFi connection work reliably so I am removing it
    for now. Maybe one day I will try it again.

--- wioterminal/wioterminal.ino
+++ wioterminal/wioterminal.ino
@@ -1,62 +1,17 @@
 #include <Wire.h>
 #include "SHT31.h"
 
-#include <rpcWiFi.h>
-#include <WebServer.h>
-
 #include <ArduinoJson.h>
-
-#include "arduino_secrets.h"
 
 /*
  * Required libraries:
  * ArduinoJson
  * Grove_SHT31_Temp_Humi_Sensor
- * Seeed_Arduino_rpcWiFi
  */
-
-// Network SSID
-char ssid[] = SECRET_SSID;
-// Network password
-char password[] = SECRET_PASS;
-// WiFi radio status
-int status = WL_IDLE_STATUS;
 
 SHT31 sht31 = SHT31();
 
 const int ledPin = 13;
-
-WebServer server(80);
-
-void printNetworkData() {
-  Serial.println("Board information:");
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  Serial.println();
-  Serial.println("Network information:");
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  long rssi = WiFi.RSSI();
-  Serial.print("Signal strength (RSSI): ");
-  Serial.println(rssi);
-}
-
-void connectToWifi() {
-  Serial.print("Connecting to WiFi network: ");
-  Serial.println(ssid);
-
-  while (status != WL_CONNECTED) {
-    Serial.println("Connecting to WiFi");
-    status = WiFi.begin(ssid, password);
-
-    delay(4000);
-  }
-  Serial.println("Connected to the WiFi network");
-  printNetworkData();
-}
 
 // Reads the sensors and returns values as JSON
 String readSensors() {
@@ -92,10 +47,6 @@ String readSensors() {
   return output;
 }
 
-void sendSensorData() {
-  server.send(200, "application/json", readSensors());
-}
-
 void setup(void) {
   pinMode(A0, INPUT);
   pinMode(WIO_LIGHT, INPUT);
@@ -107,17 +58,10 @@ void setup(void) {
   while(!Serial);
 
   sht31.begin();
-
-  connectToWifi();
-
-  server.on("/", sendSensorData);
-  server.begin();
-  Serial.println("HTTP server started");
 }
 
 void loop(void) {
   Serial.println(readSensors());
-  server.handleClient();
 
-  delay(1000);
+  delay(1500);
 }
