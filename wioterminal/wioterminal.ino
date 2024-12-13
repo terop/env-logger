4c47b6c114f8281419c2065f6b4326a1924873fa
Tero Paloheimo <tero.paloheimo@iki.fi>
2024-12-13 19:08:37 +0200

    wioterminal: add CO2 measurement support
    This is done with a SDC41 based sensor.
    Also drop SHT31 sensor support.

--- wioterminal/wioterminal.ino
+++ wioterminal/wioterminal.ino
@@ -1,22 +1,27 @@
 #include <Wire.h>
-#include "SHT31.h"
+#include <SensirionI2CScd4x.h>
 
 #include <ArduinoJson.h>
 
 /*
  * Required libraries:
  * ArduinoJson
- * Grove_SHT31_Temp_Humi_Sensor
+ * https://github.com/Sensirion/arduino-core
+ * https://github.com/Sensirion/arduino-i2c-scd4x
  */
 
-SHT31 sht31 = SHT31();
+SensirionI2CScd4x scd4x;
 
 const int ledPin = 13;
 
 // Reads the sensors and returns values as JSON
 String readSensors() {
+  JsonDocument doc;
+  String output;
+
   digitalWrite(ledPin, 1);
 
+  // Light measurement
   const uint8_t lightMeasCount = 5;
   int lightValuesSum = 0;
 
@@ -24,21 +29,47 @@ String readSensors() {
     delay(50);
     lightValuesSum += analogRead(A0);
   }
-
-  float temperature = sht31.getTemperature();
-  float humidity = sht31.getHumidity();
   int lightValue = round((float)lightValuesSum / lightMeasCount);
-  int builtInLightValue = analogRead(WIO_LIGHT);
 
-  delay(400);
-
-  DynamicJsonDocument doc(96);
-  String output;
-
-  doc["temperature"] = temperature;
-  doc["humidity"] = humidity;
   doc["light"] = lightValue;
-  doc["builtInLight"] = builtInLightValue;
+
+  // Other measurements
+  uint16_t error;
+  char errorMessage[256];
+
+  uint16_t co2 = 0;
+  float temperature = 0.0f;
+  float humidity = 0.0f;
+  bool isDataReady = false;
+  bool isInvalidMeasurement = false;
+
+  error = scd4x.getDataReadyFlag(isDataReady);
+  if (error) {
+    Serial.print("Error trying to execute getDataReadyFlag(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+  if (isDataReady) {
+    error = scd4x.readMeasurement(co2, temperature, humidity);
+    if (error) {
+      Serial.print("Error trying to execute readMeasurement(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+    } else if (co2 == 0) {
+      Serial.println("Invalid sample detected, skipping");
+      isInvalidMeasurement = true;
+    }
+  }
+
+  if (error || isInvalidMeasurement) {
+    doc["temperature"] = 0;
+    doc["humidity"] = 0;
+    doc["co2"] = 0;
+  } else {
+    doc["temperature"] = temperature;
+    doc["humidity"] = humidity;
+    doc["co2"] = co2;
+  }
 
   serializeJson(doc, output);
 
@@ -49,19 +80,42 @@ String readSensors() {
 
 void setup(void) {
   pinMode(A0, INPUT);
-  pinMode(WIO_LIGHT, INPUT);
   pinMode(ledPin, OUTPUT);
 
   digitalWrite(ledPin, 1);
 
   Serial.begin(115200);
-  while(!Serial);
+  while (!Serial) {
+    delay(100);
+  }
 
-  sht31.begin();
+  Wire.begin();
+
+  uint16_t error;
+  char errorMessage[256];
+
+  scd4x.begin(Wire);
+
+  // Stop potentially previously started measurement
+  error = scd4x.stopPeriodicMeasurement();
+  if (error) {
+    Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+
+  // Start SDC41 measurement
+  error = scd4x.startPeriodicMeasurement();
+  if (error) {
+    Serial.print("Error trying to execute startPeriodicMeasurement(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
 }
 
 void loop(void) {
   Serial.println(readSensors());
 
-  delay(1500);
+  // Long delay here is needed to get correct values for the SDC41 sensor
+  delay(5000);
 }
