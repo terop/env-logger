8d8c1a3caf3c599ac99d4c82d7f795ea8698fe18
Tero Paloheimo <tero.paloheimo@iki.fi>
2025-06-28 14:52:39 +0300

    scripts: add run check for ruff

--- scripts/verification.sh
+++ scripts/verification.sh
@@ -22,17 +22,21 @@ fi
 
 echo 'Running cljfmt'
 if [ "${CI}" ]; then
-    clojure -Ttools install io.github.weavejester/cljfmt '{:git/tag "0.13.0"}' :as cljfmt
+    clojure -Ttools install io.github.weavejester/cljfmt '{:git/tag "0.13.1"}' :as cljfmt
 fi
 clojure -Tcljfmt check
 
-echo 'Running ruff for Python files'
-if [ "${CI}" ]; then
-    apt-get install -y python3.11-venv
-    # shellcheck disable=SC1091
-    python3 -m venv .venv && . .venv/bin/activate && pip3 install ruff && ruff check
-else
-    ruff check
+# Only run ruff when called from the env-logger repository to avoid ruff failures
+# when called from other repositories
+if [ $(basename $(pwd)) = 'env-logger' ]; then
+    echo 'Running ruff for Python files'
+    if [ "${CI}" ]; then
+        apt-get install -y python3.11-venv
+        # shellcheck disable=SC1091
+        python3 -m venv .venv && . .venv/bin/activate && pip3 install ruff && ruff check
+    else
+        ruff check
+    fi
 fi
 
 # Only run ESLint locally due to difficulties to install it on CI machines
