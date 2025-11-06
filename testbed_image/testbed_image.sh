e34b5a38af609fb7bd5486f745401636a582e7ee
Tero Paloheimo <tero.paloheimo@iki.fi>
2025-11-06 16:18:17 +0200

    testbed image: use OpenID auth in image name storage
    Also fix and ignore some shellcheck warnings.

--- testbed_image/testbed_image.sh
+++ testbed_image/testbed_image.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# shellcheck disable=SC2154
+
 # A script for uploading a FMI Testbed image to a remote server.
 #
 # Copyright (C) 2017-2019 Tero Paloheimo
@@ -10,12 +12,18 @@ if [ ! -e ${conf_file} ]; then
     echo "Configuration file '${conf_file}' not found, exiting" >&2
     exit 1
 fi
+# shellcheck disable=SC1090
 . ./${conf_file}
 
 ssh_con_string="${target_user}@${target_host}"
 
 if [ -z "$(command -v uv)" ]; then
     echo "uv is not in PATH, exiting" >&2
+    exit 1
+fi
+
+if [ -z "$(command -v jq)" ]; then
+    echo "jq is not in PATH, exiting" >&2
     exit 1
 fi
 
@@ -41,7 +49,23 @@ fi
 rm "${image_name}"
 
 # Add image name to env-logger database
-if [ ${env_logger_integration} -ne 0 ]; then
-    curl -s -S --data-urlencode "name=${image_name}" --data-urlencode "code=${auth_code}" \
+if [ "${env_logger_integration}" -ne 0 ]; then
+    access_token=$(curl -s --location "${oid_token_endpoint}" \
+                        --header 'Content-Type: application/x-www-form-urlencoded' \
+                        --data-urlencode 'grant_type=client_credentials' \
+                        --data-urlencode "client_id=${oid_client_id}" \
+                        --data-urlencode "client_secret=${oid_client_secret}"|jq '.access_token'|tr -d '"')
+    # shellcheck disable=SC2181
+    if [ $? -ne 0 ]; then
+        echo "$(date -Iminutes): error: access token fetch failed, RC ${?}, exiting" >&2
+        exit 1
+    fi
+
+    if [ -z "${access_token}" ]; then
+        echo "$(date -Iminutes): error: invalid access token, exiting" >&2
+        exit 1
+    fi
+
+    curl -s -S -H "Bearer: ${access_token}" --data-urlencode "name=${image_name}" \
          "${env_logger_url}/obs/tb-image" 1>/dev/null
 fi
